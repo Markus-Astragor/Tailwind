41f77d839662cc6b1c4f40560f8d0f7c1c8902c8
Marko Kosyk <kosyk.markian@gmail.com>
2023-06-25 23:19:07 +0300

    without script

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
        <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://cdn.tailwindcss.com"></script>
     <title>Utility First Fundamentals</title>
       <body>
     {/* <!-- HTML/CSS Version --> */}
@@ -24,7 +23,9 @@ function App() {
 
     {/* <!-- Tailwind Version --> */}
 
-    
+    <div className='flex '>
+
+    </div>
    
     </div>
   );
