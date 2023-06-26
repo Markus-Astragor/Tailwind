b171d5deb3fbdce0122b014248a7fe982b841de8
Marko Kosyk <kosyk.markian@gmail.com>
2023-06-26 18:00:49 +0300

    colours start exercise

--- src/App.js
+++ src/App.js
@@ -2,8 +2,55 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <div className='font-bold text-center'>Hello Nazarchik</div>
+    <div className="text-center">
+      <body>
+        {/* <!-- Default colors --> */}
+        <p className='text-red-500 font-bold'>Tailwind is awesome</p>
+        {/* --------------------------------------------- Custom colors --------------------------*/}
+        <p className='text-yellowSichmine font-bold'>Sichmine color test</p>
+        <p className='text-blackSichmine font-bold font-bold'>This is the black Sichmine's color</p>
+        {/* <!-- white, black, red, green, blue, orange, yellow, purple, lime, emerald, teal, cyan, indigo, violet, fuchsia, pink, rose, sky, gray, slate, zinc, neutral, stone, amber,  --> */}
+        <p className='text-amber-400 font-bold'>This is the Amber color i found it beautiful</p>
+        <p className='text-yellow-500 font-bold'>I suppose yellow-500 is also interseting and good-looking</p>
+        {/* <!-- Text Colors --> */}
+        <p className='text-lime-500 font-bold'>This is the text lime-500</p>
+        <p className='text-lime-400 font-bold'>This is the text lime-400</p>
+        <p className='text-emerald-500 font-bold'>This is the emerald-500 color </p>
+        <p className='text-emerald-400 font-bold'>This is the best emerald-400 color</p>
+        <p className='text-teal-500 font-bold'>This is the teal color of text one of the most beautiful</p>
+        <p className='text-teal-400 font-bold'>This is the teal color of text is also one of the most beautiful</p>
+        <p className='text-cyan-500 font-bold'>This is the cyan color really good</p>
+        <p className='text-cyan-400 font-bold'>This is the cyan-400 color i really like it</p>
+        <p className='text-sky-500 font-bold text-2xl'>This is the sky color that is really appeals to me</p>
+        <p className='text-violet-700 font-bold text-3xl'>This is my favourite color. I really like it because in Timeactions Markus had this type of colour on the wings</p>
+        <p className='text-purple-600 font-bold text-3xl'>This is the purple color which is similiar to the previous one</p>
+        <p className='text-pink-600 text-3xl font-bold'>This is the pink color and it is supposed to be girls' color but i also like it</p>
+        <p className='text-rose-600 text-3xl font-bold'>This is the rose colour</p>
+        {/* <!-- Background Colors --> */}
+        <p className='w-1/3 bg-purple-500 m-auto'>Background color</p>
+        <p className='w-1/3 bg-violet-500 m-auto'>Background color</p>
+        <p className='w-1/3 bg-yellow-500 m-auto'>Background color</p>
+        <p className='w-1/3 bg-amber-500 m-auto'>Background color</p>
+        <p className='w-1/3 bg-yellowSichmine m-auto'>Background color</p>
+        <p className='w-1/3 bg-blackSichmine m-auto text-white'>Background color</p>
+        {/* <!-- Text Underline --> */}
+        <p className='underline decoration-red-500 underline-offset-8'>This is the decorated and underline text</p>
+        <p className='underline decoration-purple-600 underline-offset-4'>This is the underlined 4 text</p>
+        <p className='underline decoration-yellowSichmine underline-offset-3'>This is the Sichmine's underlined color</p>
+        <p className='underline decoration-cyan-500 underline-offset-2'>This is the cyan colour</p>
+        {/* <!-- Border Colors --> */}
+
+        {/* <!-- Divide Colors --> */}
+
+        {/* <!-- Outline Colors --> */}
+
+        {/* <!-- Box Shadow Colors (Opacity defaults to 100, but you can set it)--> */}
+
+        {/* <!-- Accent Colors --> */}
+
+        {/* <!-- Arbitrary Colors --> */}
+
+      </body>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellowSichmine: '#FACC15',
+        blackSichmine: '#212425'
+      }
+    },
   },
   plugins: [],
 }
